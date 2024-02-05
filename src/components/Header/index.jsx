c164a1add8eed7c31035c069909382d4cca7f547
xandoalka <xandoalka@gmail.com>
2024-02-05 08:45:58 +0700

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import Logo from '../../assets/Logo.svg';
+import styles from './header.module.css';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from 'react';
+
+export function Header({ handleAddTask }) {
+  const [title, setTitle] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleAddTask(title);
+    setTitle('');
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <img src={Logo} />
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input placeholder="Add a new task" type="text" onChange={onChangeTitle} value={title} />
+        <button>Create <AiOutlinePlusCircle size={20} /></button>
+      </form>
+    </header>
+  )
+}
